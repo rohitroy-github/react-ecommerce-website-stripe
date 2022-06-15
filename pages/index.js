74ce27521cacacbdf69bb30de226ce2f39a9295e
rohit <rohitdevops.roy@gmail.com>
2022-06-15 21:38:18 +0530

    Adding FooterBanner.jsx

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ const Home = ({ products, bannerData }) => {
       {products?.map( (product) => <Product key={product._id} product={product} /> )}
     </div>
 
-    <FooterBanner />
+    <FooterBanner footerBanner = {bannerData && bannerData[0]} />
     </>
   )
 }
