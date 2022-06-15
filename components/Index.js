7d0f7720c4c57622119ce87695c9b35caba4291c
rohit <rohitdevops.roy@gmail.com>
2022-06-15 18:16:21 +0530

    Adding components

---
+++ components/Index.js
@@ -0,0 +1,11 @@
+export { default as Footer } from './Footer'; 
+
+export { default as Navbar } from './Navbar'; 
+
+export { default as Cart } from './Cart'; 
+
+export { default as Layout } from './Layout'; 
+
+export { default as HeroBanner } from './HeroBanner'; 
+
+export { default as FooterBanner } from './FooterBanner'; 
