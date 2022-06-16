08e3e334c4a3619faaba11b02df60f0f1964bd13
rohit <rohitdevops.roy@gmail.com>
2022-06-16 14:05:23 +0530

    Updating [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,66 +1,131 @@
 // Putting a [name] makes the file dynamic
 // File based routing
 
-import React from "react";
+import React, { useState } from "react";
+
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
 
 import { client, urlFor } from "../../lib/client";
 
-const ProductDetails = (product, products) => {
-    const { image, name, details, price } = product; 
+import { Product } from "../../components";
+
+const ProductDetails = ({ product, products}) => {
+
+  const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
   return (
-    <div> 
-      <div className="product-detail-container"> 
-        <div> 
-          <div className="image-container"> 
-            <img src={urlFor(image && image[0])} />
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+            />
           </div>
-          {/* Image Carousel Code */}
-          <div className="small-images-conatiner">
-            {image?.map((item, i) => ( 
-              <img 
-              src={urlFor(item)}
-              className=""
-              onMouseEnter=""
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img
+                key={i}
+                src={urlFor(item)}
+                className=
+                {i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
               />
             ))}
-          </div> 
+          </div>
+        </div>
+
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details: </h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick="">
+                <AiOutlineMinus />
+              </span>
+              <span className="num">0</span>
+              <span className="plus" onClick="">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onClick="">
+              Add to Cart
+            </button>
+            <button type="button" className="buy-now" onClick="">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+          {products.map((item) => (
+                <Product key={item._id} product={item} />
+              ))}
+          </div>
         </div>
       </div>
     </div>
-  )
-}
-
-export const getStaticPaths = async () => {
-    const query = `*[_type == "product"] {
-      slug {
-        current
-      }
-    }
-    `;
-  
-    const products = await client.fetch(query);
-  
-    const paths = products.map((product) => ({
-      params: { 
-        slug: product.slug.current
-      }
-    }));
-  
-    return {
-      paths,
-      fallback: 'blocking'
-    }
-  }
+  );
+};
 
 // getStaticProps function is used when the data required to render the page is available at runtime ahead of user request
 
-export const getStaticProps = async ({ params: { slug } }) => {
-  //To fetch product details from thne product page we are on currently
-  const query = `*_type == "product" && slug.current == '${slug}'][0]`; //Fetching a particular product requested
-  const productQuery = `*[_type == "product"]`; //Fetching similar products
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+    slug {
+      current
+    }
+  }
+  `;
 
-  const product = await client.fetch(query); 
-  const products = await client.fetch(productQuery); 
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+//To fetch product details from thne product page we are on currently
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`; //Fetching a particular product requested
+  const productsQuery = '*[_type == "product"]'; //Fetching similar products
+
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+
+  console.log(product);
 
   return {
     props: { product, products },
